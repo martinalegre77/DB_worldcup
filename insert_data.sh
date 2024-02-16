18148041685661017cc1dcf09aa6d407469db944
martinalegre77 <115106085+martinalegre77@users.noreply.github.com>
2024-02-16 17:26:11 -0300

    Create insert_data.sh

---
+++ insert_data.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "TRUNCATE teams, games")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS 
+do
+  # WINNER
+  if [[ $WINNER != "winner" ]]
+  then
+    # get team_id
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    # if not found
+    if [[ -z $WINNER_ID ]]
+    then
+      # insert name
+      INSERT_NAME_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_NAME_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into TEAMS, $WINNER
+      fi
+      # get new team_id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi  
+  fi
+  # OPPONENT
+  if [[ $OPPONENT != "opponent" ]]
+  then
+    # get opponent_id
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    # if not found
+    if [[ -z $OPPONENT_ID ]]
+    then
+      # insert name
+      INSERT_NAME_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_NAME_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into TEAMS, $OPPONENT
+      fi
+      # get new opponent_id
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi 
+  fi
+
+  # insert into games
+  INSERT_GAMES=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) 
+  VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  if [[ $INSERT_GAMES == "INSERT 0 1" ]]
+  then
+    echo Inserted into games, $YEAR : $ROUND : $WINNER_ID : $WINNER_GOALS - $OPPONENT_ID : $OPPONENT_GOALS
+  fi
+done
